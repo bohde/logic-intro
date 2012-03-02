fdf9af11850b3456e32e5b40239a911a2a7aa7ba
Josh Bohde <josh.bohde@gmail.com>
2012-03-01 23:54:38 -0600

    Add some list methods.

--- src/logic_intro/core.clj
+++ src/logic_intro/core.clj
@@ -10,6 +10,9 @@
 (run* [q]
       succeed)
 
+(run* [q]
+      (== 'apple 'banana))
+
 ;; What are all the values `q` where `q` unifies to 'apple? 
 (run* [q]
       (== q 'apple))
@@ -44,3 +47,31 @@
       (conde
        [(== 'apple q)]
        [(== 'banana q)]))
+
+
+;; firsto is like first, but for logical variables
+(run* [q]
+      (firsto '(a p p l e) q))
+
+(run* [q]
+      (firsto (list q 'p 'p 'l 'e) 'a))
+
+
+;; resto is like rest, but for logical variables
+(run* [q]
+      (resto '(a p p l e) q))
+
+(run* [q]
+      (resto q '(p p l e)))
+
+
+;; membero is like member
+(run* [q]
+      (membero 'apple '(apple banana cherry))
+      (== true q))
+
+
+(run* [q]
+      (fresh [x y z]
+             (== (list x y z) q)
+             (membero 'apple q)))
